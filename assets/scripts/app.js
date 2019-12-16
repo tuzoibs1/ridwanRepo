754f35b58ab6736ea865d6b320a65466ac162950
Ridwan Muslih <tuzoibsa@gmail.com>
2019-12-15 19:12:09 -0600

    adding new image and loop my header text

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -60,18 +60,17 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a Full-Stack Web Developer.", "love everything about programming.", "also teach programming to people.", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
-			typeSpeed: 20,
-			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-			},
+			typeSpeed: 25,
+			loop: true,
+			contentType: 'text',		
 			preStringTyped: function() {},
-			onStringTyped: function() {}
+			onStringTyped: function() {},
+			loopDelay: 80
 		});
 	}
 
